6776e22636a53f32891799278ca6f7791acfcbd3
lvpin233 <lvpin233@163.com>
2019-08-10 14:54:00 +0800

    init

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Lvpin
+ * @date 2019/8/10 12:19
+ */
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+    @RequestMapping("/demo")
+    public Object demo() {
+        return "demo";
+    }
+}
